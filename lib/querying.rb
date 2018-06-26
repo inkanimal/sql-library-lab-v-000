e4dc01e2bbeb3836638f75f524facddb693b03a6
geoffrey woodcock <theatomproject@gmail.com>
2018-06-26 15:59:52 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,8 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT species AS value  FROM characters
-   GROUP BY species"
+   GROUP BY species
+   COUNT species"
 end
 
 def select_name_and_series_subgenres_of_authors
